9c41f608bb3aaaeada7d206930135b4356ac4cae
Miciah Henderson <48624788+kiahjh@users.noreply.github.com>
2025-07-23 16:08:50 -0400

    SAVEPOINT

--- examples/test.asm
+++ examples/test.asm
@@ -3,32 +3,35 @@ global _start
 
 section .rodata
 
-  get_three#0: dq FUN#0
-  main#1: dq FUN#1
+  main#1: dq FUN#0
 
 section .text
 
 _start:
-  ; goto FUN#1
-  jmp FUN#1
+  ; goto FUN#0
+  jmp FUN#0
 
 
 FUN#0:
+  enter 0, 0
+
+  ; get_three#0 := FUN#1
+  push qword FUN#1
+
+  ; t2 := get_three#0()
+  call [rbp - 8]
+  push qword rax
+
+  ; exit t2
+  mov rax, 60
+  mov rdi, [rbp - 16]
+  syscall
+
+
+FUN#1:
   enter 0, 0
 
   ; ret 3
   mov rax, 3
   leave
   ret
-
-FUN#1:
-  enter 0, 0
-
-  ; t2 := get_three#0()
-  call [get_three#0]
-  push qword rax
-
-  ; exit t2
-  mov rax, 60
-  mov rdi, [rbp - 8]
-  syscall
